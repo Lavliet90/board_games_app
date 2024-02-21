736138973803b90634d0b2bc2a5fb1bc3ef26fc2
Lavliet <bbb.zzzz.bbb@mail.ru>
2024-02-21 15:39:39 +0100

    init, add model User, BoardGames

--- user/models.py
+++ user/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=70)
     nickname = models.CharField(max_length=20)
-    boardgames = models.ManyToManyField('BoardGame', related_name='users')
+    boardgames = models.ManyToManyField('BoardGames', related_name='users')
     friends = models.ManyToManyField('self', related_name='friends', symmetrical=False)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
